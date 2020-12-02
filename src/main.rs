ea1b893644c5cd10b6db0740450e7ff300578175
EugeniaFranchi <mfranchi@fi.uba.ar>
2020-12-02 11:19:57 -0300

    cajero con condición de corte

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ fn main() {
     let park_config = config::read_configuration("./config/config.yml");
     println!("{:?}", park_config);
     
-    let mut parque = parque::Parque{caja: 0.0};
+    let mut parque = parque::Parque{caja: 0.0, abierto: true};
     parque.abrir();
 }
--- src/parque/cajero.rs
+++ src/parque/cajero.rs
@@ -2,15 +2,18 @@ use std::{thread, time};
 
 pub struct Cajero {
     pub intervalo: time::Duration,
-    pub lock_caja: std::sync::Arc<std::sync::RwLock<f32>>
+    pub lock_caja: std::sync::Arc<std::sync::RwLock<f32>>,
+    pub lock_parque_abierto: std::sync::Arc<std::sync::RwLock<bool>>
 }
 
 impl Cajero {
     pub fn iniciar(&mut self){
-        for _ in 0..5 {
+        while *self.lock_parque_abierto.read().expect("Error al leer valor de estado del parque") {
             thread::sleep(self.intervalo);
             let caja = self.lock_caja.read().expect("Error al leer valor de caja");
             println!("Caja: {:?}", *caja);
         }
+        let caja = self.lock_caja.read().expect("Error al leer valor de caja");
+        println!("Valor caja final: {:?}", *caja);
     }
 }
