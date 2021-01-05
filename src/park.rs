fbfd064e20adbb9cebf726d1a4e80ec3bae260d9
Franco Cerquetti <fd.cerquetti@gmail.com>
2021-01-05 19:15:40 -0300

    Refactor

--- src/park.rs
+++ src/park.rs
@@ -11,61 +11,102 @@ static MSG_ERROR_OPEN_W: &str = "Error writing park state.";
 static MSG_ERROR_JOIN: &str = "Error joining thread.";
 
 pub struct Park {
-    pub cash: f32,
-    pub is_open: bool,
-    pub park_config: ParkConfig
+    cash: f32,
+    park_config: ParkConfig,
+    lock_is_open: Arc<RwLock<bool>>,
+    games_threads: Option<Vec<thread::JoinHandle<()>>>,
+    cashier_thread: Option<thread::JoinHandle<()>>,
+    cash_mutex: Option<Arc::<Mutex<f32>>>
 }
 
 impl Park {
-    fn initialize_cashier(&mut self, o_lock: std::sync::Arc<std::sync::RwLock<bool>>)
-     -> (std::sync::Arc<std::sync::Mutex<f32>>, std::thread::JoinHandle<()>){
+    pub fn new(cash: f32, park_config: ParkConfig) -> Park {
+        Park {
+            cash: cash, 
+            park_config: park_config,
+            lock_is_open: Arc::new(RwLock::new(false)),
+            games_threads: None,
+            cashier_thread: None,
+            cash_mutex: None
+        }
+    }
+
+    pub fn simulate_customer(&mut self, num: i64) {
+        // thread::sleep(time::Duration::from_secs(2));
+        println!("Sim {} start", num);
+        if let Some(mutex) = self.cash_mutex.take() {
+            let mut cash = mutex.lock().unwrap();
+            *cash += 10.0;
+            println!("Sim {}, {}", num, cash);
+        }
+    }
+
+    fn initialize_cashier(&mut self, o_lock: Arc<RwLock<bool>>) -> (Arc<Mutex<f32>>, thread::JoinHandle<()>) {
         let mutex_cash = Arc::new(Mutex::new(self.cash));
         let c_mutex = mutex_cash.clone();
         let c_thread = thread::spawn(move || {
-            let mut cashier = cashier::Cashier{interval: time::Duration::from_secs(CASHIER_INTERVAL),
-                                            mutex_cash: c_mutex,
-                                            lock_park_is_open: o_lock};
+            let mut cashier = cashier::Cashier {
+                interval: time::Duration::from_secs(CASHIER_INTERVAL),
+                mutex_cash: c_mutex,
+                lock_park_is_open: o_lock
+            };
             cashier.iniciar();
         });
         (mutex_cash, c_thread)
     }
 
-    fn initialize_game(&mut self, o_lock: std::sync::Arc<std::sync::RwLock<bool>>, number: usize,
-    c_mutex: std::sync::Arc<std::sync::Mutex<f32>>)-> std::thread::JoinHandle<()> {
+    fn initialize_game(&mut self, o_lock: Arc<RwLock<bool>>, number: usize, c_mutex: Arc<Mutex<f32>>) -> thread::JoinHandle<()> {
         let cost = self.park_config.games_cost[number];
         let g_thread = thread::spawn(move || {
-            let mut game = game::Game{mutex_cash: c_mutex,
-                                      duration: time::Duration::from_secs(GAME_DURATION),
-                                      cost: cost,
-                                      lock_park_is_open: o_lock,
-                                      flaw_prob: GAME_FLAW_PROB};
+            let mut game = game::Game {
+                mutex_cash: c_mutex,
+                duration: time::Duration::from_secs(GAME_DURATION),
+                cost: cost,
+                lock_park_is_open: o_lock,
+                flaw_prob: GAME_FLAW_PROB
+            };
+            
             game.switch_on();
         });
         g_thread
     }
 
     pub fn open(&mut self){
-        let lock_is_open = Arc::new(RwLock::new(self.is_open));
-        let mut games = Vec::new();
-
-        //Inicio de caja
-        let (mutex_cash, c_thread) = self.initialize_cashier(lock_is_open.clone());
-        //Inicio de juegos
-        for i in 0..self.park_config.number_of_games {
-            games.push(self.initialize_game(lock_is_open.clone(), i, mutex_cash.clone()));
+        {
+            let mut open_lock = self.lock_is_open.write().expect(MSG_ERROR_OPEN_W);
+            *open_lock = true;
         }
 
-        //Duermo un rato para que se mueva la caja y cierro el parque
-        thread::sleep(time::Duration::from_secs(6));
+        //Inicio de caja
+        let (mutex_cash, c_thread) = self.initialize_cashier(self.lock_is_open.clone());
+        self.cashier_thread = Some(c_thread);
+        self.cash_mutex = Some(mutex_cash.clone());
+        
+        //Inicio de juegos
+        let mut games = Vec::new();
+        for i in 0..self.park_config.number_of_games {
+            let game = self.initialize_game(self.lock_is_open.clone(), i, mutex_cash.clone());
+            games.push(game);
+        }
+        self.games_threads = Some(games);
+    }
+    
+    pub fn close(&mut self) {
         {
-            let mut is_open = lock_is_open.write().expect(MSG_ERROR_OPEN_W);
+            let mut is_open = self.lock_is_open.write().expect(MSG_ERROR_OPEN_W);
             *is_open = false;
         }
 
-        for game in games {
-            game.join().expect(MSG_ERROR_JOIN);
+        // Espero por los juegos
+        if let Some(games) = self.games_threads.take() {
+            for game in games {
+                game.join().expect(MSG_ERROR_JOIN);
+            }
         }
-        c_thread.join().expect(MSG_ERROR_JOIN);
+
+        // Espero por el cajero
+        if let Some(handle) = self.cashier_thread.take() {
+            handle.join().expect(MSG_ERROR_JOIN);
+        }
     }
-    
 }
